ae623395c0114198a747d8282c70d93735cf3fda
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-06-14 10:26:57 -0400

    Added dotnet artifacts for vscode

--- GameDiaryProj/obj/Debug/net8.0/GameDiaryProj.AssemblyInfo.cs
+++ GameDiaryProj/obj/Debug/net8.0/GameDiaryProj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDiaryProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65652299c2a62cda3edd4fcdc2feb036d98ea3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDiaryProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDiaryProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
